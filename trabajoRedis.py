6aa82ee4f7952d36c14bfca3e1d15105231c64b1
Federico Luque <fluque87@gmail.com>
2025-01-08 20:40:59 +0100

    Inicio ejercicio Redis

---
+++ trabajoRedis.py
@@ -0,0 +1,6 @@
+import redis
+
+# Conexion
+conexionRedis = redis.ConnectionPool(host='localhost', port=6379, db=0,decode_responses=True)
+baseDatosRedis = redis.Redis(connection_pool=conexionRedis)
+
