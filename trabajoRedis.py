254d4398fc27f17cc9ffbed93003362bfbdef933
federico <fluque87@gmail.com>
2025-01-12 20:25:49 +0100

    Ejercicio 5

--- trabajoRedis.py
+++ trabajoRedis.py
@@ -16,3 +16,24 @@ baseDatosRedis.set('barco_7', 'EcoMarine')
 
 # 2 - Obtener y mostrar el número de claves registradas (0.5 puntos)
 
+num_claves = baseDatosRedis.dbsize()
+
+# Mostrar el resultado
+print(f"Número de claves en Redis: {num_claves}")
+
+# 3 - Obtener y mostrar un registro en base a una clave (0.5 puntos)
+
+barco_1 = baseDatosRedis.get('barco_1')
+
+# Mostrar el resultado
+print(f"Barco 1: {barco_1}")
+
+# 4 - Actualizar el valor de una clave y mostrar el nuevo valor(0.5 puntos)
+
+baseDatosRedis.set('barco_1', 'BlueWave 2.0')
+barco_1 = baseDatosRedis.get('barco_1')
+
+# Mostrar el resultado
+print(f"Barco 1: {barco_1}")
+
+# 5 - Eliminar una clave-valor y mostrar la clave y el valor eliminado(0.5 puntos)
